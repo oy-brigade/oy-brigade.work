e4b1da97931a4853e03fb0b0a3f839a435835b21
Kanon Kakuno <yadex205@outlook.jp>
2018-11-04 16:57:13 +0900

    :+1: Support dynamic routing for /news/:slug

--- src/functions/index.js
+++ src/functions/index.js
@@ -1,5 +1,7 @@
 const { createServer } = require('http');
 const { resolve } = require('path');
+const { parse } = require('url');
+const pathToRegexp = require('path-to-regexp');
 const functions = require('firebase-functions');
 
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
@@ -9,15 +11,27 @@ const NEXT_CONF = {
   dir: IS_DEVELOPMENT ? resolve(__dirname, '../app') : undefined,
   conf: !IS_DEVELOPMENT ? { distDir: 'app' } : undefined
 };
+const REGEXP_PATH_NEWS = pathToRegexp('/news/:slug');
 
 let app;
 let handle;
+const router = (app, handle) => (req, res) => {
+  const parsedUrl = parse(req.url, true);
+  const { pathname, query } = parsedUrl;
+  let matches = null;
+
+  if ((matches = REGEXP_PATH_NEWS.exec(pathname))) {
+    app.render(req, res, '/news/entry', query);
+  } else {
+    handle(req, res, parsedUrl);
+  }
+};
 
 if (IS_DEVELOPMENT) {
   app = require('next')(NEXT_CONF);
   handle = app.getRequestHandler();
   app.prepare().then(() => {
-    const server = createServer(handle);
+    const server = createServer(router(app, handle));
     server.listen(PORT, () => {
       console.log(`Next.js dev server is listening on http://localhost:${PORT}`)
     });
@@ -27,5 +41,5 @@ if (IS_DEVELOPMENT) {
 exports.next = functions.https.onRequest((req, res) => {
   app = app || require('next')(NEXT_CONF);
   handle = handle || app.getRequestHandler();
-  return app.prepare().then(() => handle(req, res));
+  return app.prepare().then(() => router(app, handle)(req, res));
 });
