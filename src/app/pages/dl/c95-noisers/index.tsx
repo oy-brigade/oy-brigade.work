263bbf728ceb0e78c64e0835ba944b481cbe0985
Kanon Kakuno <yadex205@outlook.jp>
2018-12-30 01:09:14 +0900

    :+1:

--- src/app/pages/dl/c95-noisers/index.tsx
+++ src/app/pages/dl/c95-noisers/index.tsx
@@ -4,24 +4,47 @@ import Head from 'next/head';
 
 import 'static/css/app.styl';
 
-export default () => (
-  <>
-    <Head>
-      <title>Noisers (C95頒布) | Downloads | OY旅団/OY brigade</title>
-    </Head>
+interface Props {};
 
-    <section className="o-dl-section">
-      <div className="o-dl-section__inner">
-        <div className="o-dl-section__title">C95頒布 Noisers ダウンロードページ</div>
-        <form className="m-dl-form">
-          <div className="m-dl-form__input-container">
-            <input type="text" placeholder="ダウンロードコード" />
+export default class extends React.Component<Props> {
+  inputRef: React.RefObject<HTMLInputElement>;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.inputRef = React.createRef();
+  }
+
+  handleClick() {
+    const pass = this.inputRef.current.value;
+    if (pass === 'm5EG12eP') {
+      location.href = 'https://www.dropbox.com/s/5r0d3ybniiygbgj/noisers_trial_c95.zip?dl=0';
+    } else {
+      location.reload();
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <Head>
+          <title>Noisers (C95頒布) | Downloads | OY旅団/OY brigade</title>
+        </Head>
+
+        <section className="o-dl-section">
+          <div className="o-dl-section__inner">
+            <div className="o-dl-section__title">C95頒布 Noisers ダウンロードページ</div>
+            <div className="m-dl-form">
+              <div className="m-dl-form__input-container">
+                <input type="text" placeholder="ダウンロードコード" ref={this.inputRef} />
+              </div>
+              <div className="m-dl-form__button-container">
+                <button onClick={this.handleClick.bind(this)}>ダウンロード</button>
+              </div>
+            </div>
           </div>
-          <div className="m-dl-form__button-container">
-            <button>ダウンロード</button>
-          </div>
-        </form>
-      </div>
-    </section>
-  </>
-);
+        </section>
+      </>
+    );
+  }
+}
